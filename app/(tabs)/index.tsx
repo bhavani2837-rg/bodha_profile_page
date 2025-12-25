0aa6e52d2252180b8017f104827f3b87d97bd8d7
Bhavani Prasad <bhavani2837@gmail.com>
2025-12-25 19:52:33 +0530

    Refactor App component: render AdminLogin component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import AdminLogin from "./AdminLogin";
+import AdminLogin from "./AdminLogin.jsx";
 // path correct ga undali
 
 function App() {
-  return <AdminLogin />;
+  return <AdminLogin/>;
 }
 
 export default App;
